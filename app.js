24a19b47585ad4298a921150f4850563d97e08df
Luiz Fillipe <luiz.fillipe.2000@hotmail.com>
2024-05-22 13:26:14 -0300

    👌 style: alterações de revisão de código

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
 const handlebars = require("express-handlebars");
 const path = require("path");
-
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 const PORT = 3000;
 
@@ -14,23 +12,21 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // handlebars
-
 app.set("views", path.join(__dirname, "src/views"));
-app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' })); //mudança para o node novo
+app.engine("handlebars", handlebars.engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // static folder
-
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.listen(PORT, function () {
-    console.log(`O express está rodando na porta ${PORT}`);
+  console.log(`O express está rodando na porta ${PORT}`);
 });
 
-app.get('/', (req, res) => {
-    res.render("index");
+//Direcionamento para o handlebars index
+app.get("/", (req, res) => {
+  res.render("index");
 });
-
 
 //app routes
-app.use('/routes', require('./src/routes/routes'));
+app.use("/routes", require("./src/routes/routes"));
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,44 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/add", (req, res) => {
-    res.send("add");
-});
-
-
 const url = "https://api.openweathermap.org/data/2.5/weather?q=";
 const access_key = "55b29b8d621215a01d17d0e1039cf110";
-let query = "Sao Paulo";
 
 async function getWeather(city) {
-    // const response = await fetch(url + "/current?access_key=" + access_key + "&query=" + city);
-
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${access_key}&lang=pt_br`);
-
-    const data = await response.json();
-
-    // let data_weather_user = data_weather(data.current.temperature, data.current.wind_speed,
-    //     data.current.pressure, data.current.precip, data.weather_descriptions, data.location.localtime, data.location.utc_offset, data.location.name);
-    // data_weather_user.printData();
-
-    return data;
-};
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${access_key}&lang=pt_br`,
+  );
+  const data = await response.json();
+  return data;
+}
 
 router.get("/get", async (req, res) => {
-    const city = req.query.city;
-    console.log("adsad")
+  const city = req.query.city;
 
-    try {
-        const data1 = await getWeather(city);
-        res.send(data1);
-        console.log("dentro do /get");
-        console.log(data1);
-        return data1;
-    } catch (error) {
-        console.log("adsad")
-        res.status(500).send(error.message);
-    }
+  try {
+    const data = await getWeather(city);
+    res.send(data);
+    return data;
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
-
 
 module.exports = router;
